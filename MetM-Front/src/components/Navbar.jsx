2648426fbf2f015dd91ce6312dd4fec6843a4a77
thomasR35 <thomasriou.35@gmail.com>
2025-03-10 15:11:19 +0100

    Feat Navbar + Responsive

--- MetM-Front/src/components/Navbar.jsx
+++ MetM-Front/src/components/Navbar.jsx
@@ -1,17 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaBars } from "react-icons/fa"; // Icône menu burger
 import "../styles/components/_navbar.scss";
 
 const Navbar = () => {
   const { user, isAuthenticated, isAdmin, logout } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  // 🔥 Confirmation avant déconnexion
   const handleLogout = () => {
-    const confirmLogout = window.confirm(
-      "Voulez-vous vraiment vous déconnecter ?"
-    );
-    if (confirmLogout) {
+    if (window.confirm("Voulez-vous vraiment vous déconnecter ?")) {
       logout();
     }
   };
@@ -19,11 +17,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
+        <div className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+          <FaBars />
+        </div>
+
         <Link className="logo" to="/">
           Marcelle & Maurice Shop
         </Link>
 
-        <div className="nav-center">
+        <div className={`nav-center ${menuOpen ? "open" : ""}`}>
           {isAuthenticated && (
             <span className="user-info">Bonjour, {user.username}!</span>
           )}
@@ -48,10 +50,51 @@ const Navbar = () => {
             </Link>
           )}
         </div>
+
         <Link className="cart-icon" to="/cart">
           <FaShoppingCart />
         </Link>
       </div>
+
+      {menuOpen && (
+        <ul className="nav-links-mobile">
+          <li>
+            <Link
+              className="btn-gallery"
+              to="/gallery"
+              onClick={() => setMenuOpen(false)}
+            >
+              Galerie
+            </Link>
+          </li>
+
+          {!isAuthenticated ? (
+            <li>
+              <Link
+                className="btn-login"
+                to="/signup"
+                onClick={() => setMenuOpen(false)}
+              >
+                Connexion
+              </Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <button onClick={handleLogout}>Déconnexion</button>
+              </li>
+            </>
+          )}
+
+          {isAdmin && (
+            <li>
+              <Link to="/admin/dashboard" onClick={() => setMenuOpen(false)}>
+                Admin
+              </Link>
+            </li>
+          )}
+        </ul>
+      )}
     </nav>
   );
 };
