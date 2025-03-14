f77ac4a8caf09089884f6a4ac77e345c9988e7c2
thomasR35 <thomasriou.35@gmail.com>
2025-03-14 13:43:59 +0100

    MaJ Home Feat Product Pages

--- MetM-Front/src/pages/Home.jsx
+++ MetM-Front/src/pages/Home.jsx
@@ -28,8 +28,41 @@ const Home = () => {
       </section>
       <section className="product-info">
         <p>
-          En offrant votre image, vous pouvez personnaliser vos objets favoris
-          et les rendre uniques.
+          Nous nous engageons à vous fournir des produits de haute qualité,
+          fabriqués en France à partir de matériaux biosourcés et durables. Nous
+          assurons l'excellence de nos mugs, t-shirts et pins :
+        </p>
+        <h4>Mugs en céramique de haute qualité</h4>
+        <p>
+          Nos mugs sont fabriqués en faïence française, une céramique réputée
+          pour sa robustesse et son esthétisme garantissant une finition soignée
+          et une durabilité appréciable. Nous collaborons avec des manufactures
+          françaises renommées, telles que la Faïencerie de Gien, fondée en 1821
+          et située dans le Loiret, qui perpétue un savoir-faire d'exception
+          dans la production de faïence fine. Leur expertise garantit des
+          produits à la fois esthétiques et résistants, adaptés à un usage
+          quotidien.
+        </p>
+        <h4>T-shirts en coton biologique français</h4>
+        <p>
+          Nos t-shirts sont confectionnés à partir de coton biologique cultivé
+          en France, assurant une empreinte écologique réduite et une
+          traçabilité complète. Nous collaborons avec des marques engagées dans
+          le développement durable, telles que 1083, qui propose des t-shirts en
+          coton bio certifié, fabriqués intégralement en France. Cette démarche
+          garantit des textiles de qualité supérieure, doux au toucher et
+          résistants au temps.
+        </p>
+        <h4>Pins en aluminium recyclé d'origine française</h4>
+        <p>
+          Nos pins sont fabriqués à partir d'aluminium recyclé, provenant de
+          circuits de recyclage français. Cette approche permet de réduire
+          l'impact environnemental tout en assurant une qualité et une finition
+          irréprochables. Nous veillons à ce que chaque étape de production
+          respecte des normes strictes, garantissant des produits durables et
+          esthétiques.​ En choisissant nos produits, vous soutenez l'artisanat
+          français et optez pour des articles écoresponsables, alliant qualité,
+          durabilité et respect de l'environnement.​
         </p>
       </section>
     </main>
