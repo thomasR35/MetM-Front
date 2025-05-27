445df530e87fcddf8ea1ed438ba1d131b907b902
thomasR35 <thomasriou.35@gmail.com>
2025-05-27 15:11:06 +0200

    #Service
    Création de cropZones dans service/cropping

---
+++ MetM-Front/src/services/cropping/cropZones.js
@@ -0,0 +1,5 @@
+export const cropZones = {
+  mug: { x: 250, y: 320, width: 300, height: 300 },
+  tshirt: { x: 320, y: 220, width: 350, height: 350 },
+  pins: { x: 141, y: 134, width: 470, height: 470 },
+};
